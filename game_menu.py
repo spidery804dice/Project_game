c116a818eeb8e8cfe21c1ae980779138ec3c30d6
spidery804dice <garikmishkin@gmail.com>
2023-12-25 20:57:53 +0300

    Сделал настройки экрана

--- game_menu.py
+++ game_menu.py
@@ -9,7 +9,7 @@ FPS = 60
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Тест")
-background = pygame.image.load("back.jpg")
+background = pygame.image.load("back600.jpg")
 clock = pygame.time.Clock()
 
 cur = pygame.image.load("cursor.png")
@@ -19,6 +19,7 @@ pygame.mouse.set_visible(False)
 def terminate():
     pygame.quit()
     sys.exit()
+
 
 def draw_cur():
     x, y = pygame.mouse.get_pos()
@@ -34,7 +35,7 @@ def main_menu():
     running = True
     while running:
         screen.fill((0, 0, 0))
-        screen.blit(background, (0, -50))
+        screen.blit(background, (0, 0))
 
         font = pygame.font.Font(None, 72)
         text_surface = font.render("GAME", True, (0, 0, 0))
@@ -79,7 +80,7 @@ def settings_menu():
     running = True
     while running:
         screen.fill((0, 0, 0))
-        screen.blit(background, (0, -50))
+        screen.blit(background, (0, 0))
 
         font = pygame.font.Font(None, 72)
         text_surface = font.render("SETTINGS", True, (0, 0, 0))
@@ -100,10 +101,69 @@ def settings_menu():
                 fade()
                 running = False
 
+            if event.type == pygame.USEREVENT and event.button == video:
+                fade()
+                video_settings()
+
             for btn in [audio, video, back]:
                 btn.handle_event(event)
 
         for btn in [audio, video, back]:
+            btn.check_hover(pygame.mouse.get_pos())
+            btn.draw(screen)
+
+        draw_cur()
+
+        pygame.display.flip()
+
+
+def video_settings():
+    global WIDTH, HEIGHT, screen
+    video_1 = ImageButton(10, 180, 252, 74, "600x550", "Button_1-1.png", "Button_1.png", "click.mp3")
+    video_2 = ImageButton(10, 280, 252, 74, "1280x800", "Button_1-1.png", "Button_1.png", "click.mp3")
+    video_3 = ImageButton(10, 380, 252, 74, "Full HD", "Button_1-1.png", "Button_1.png", "click.mp3")
+    back = ImageButton(10, 480, 252, 74, "Назад", "Button_1-1.png", "Button_1.png", "click.mp3")
+
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        screen.blit(background, (0, 0))
+
+        font = pygame.font.Font(None, 72)
+        text_surface = font.render("SETTINGS", True, (0, 0, 0))
+        text_rect = text_surface.get_rect(center=(130, 130))
+        screen.blit(text_surface, text_rect)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                terminate()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    fade()
+                    running = False
+
+            if event.type == pygame.USEREVENT and event.button == back:
+                fade()
+                running = False
+
+            if event.type == pygame.USEREVENT and event.button == video_1:
+                change_mode(600, 550)
+                fade()
+
+            if event.type == pygame.USEREVENT and event.button == video_2:
+                change_mode(1100, 602)
+                fade()
+
+            if event.type == pygame.USEREVENT and event.button == video_3:
+                change_mode(1366, 767, pygame.FULLSCREEN)
+                fade()
+
+            for btn in [video_1, video_2, video_3, back]:
+                btn.handle_event(event)
+
+        for btn in [video_1, video_2, video_3, back]:
             btn.check_hover(pygame.mouse.get_pos())
             btn.draw(screen)
 
@@ -118,7 +178,7 @@ def new_game():
     running = True
     while running:
         screen.fill((0, 0, 0))
-        screen.blit(background, (0, -50))
+        screen.blit(background, (0, 0))
 
         font = pygame.font.Font(None, 72)
         text_surface = font.render("New game", True, (0, 0, 0))
@@ -170,6 +230,14 @@ def fade():
 
         pygame.display.flip()
         clock.tick(FPS)
+
+
+def change_mode(w, h, fullscreen=0):
+    global WIDTH, HEIGHT, screen, background
+
+    WIDTH, HEIGHT = w, h
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), fullscreen)
+    background = pygame.image.load(f"back{WIDTH}.jpg")
 
 
 if __name__ == "__main__":
